55c55dc3dc6b7b51dd88e88649c5236d819b76d9
rouf_majidin <roufmajidin777@gmail.com>
2023-05-18 00:07:50 +0700

    add color style

---
+++ lib/themes/color_style.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class ColorsTheme {
+  static final Color bgScreen = Color(0xffF6F6F6);
+}
+
+class TextTheme {
+  static TextStyle heading1 = GoogleFonts.josefinSans(
+      fontWeight: FontWeight.w600,
+      fontSize: 16,
+      color: const Color(0xff0B0B0B));
+  static TextStyle heading2 = GoogleFonts.josefinSans(
+    fontWeight: FontWeight.w600,
+    fontSize: 14,
+    color: const Color(0xff0B0B0B),
+  );
+  static TextStyle heading3 = GoogleFonts.josefinSans(
+    fontWeight: FontWeight.w500,
+    fontSize: 12,
+    color: const Color(0xff0B0B0B),
+  );
+  static TextStyle heading4 = GoogleFonts.josefinSans(
+    fontWeight: FontWeight.w400,
+    fontSize: 10,
+    color: const Color(0xff0B0B0B),
+  );
+  static TextStyle heading5 = GoogleFonts.josefinSans(
+    fontWeight: FontWeight.w600,
+    fontSize: 12,
+    color: const Color(0xff0B0B0B),
+  );
+  static TextStyle headingLabel = GoogleFonts.josefinSans(
+    fontWeight: FontWeight.w600,
+    fontSize: 10,
+    color: const Color(0xffFF7F00),
+  );
+  static TextStyle headingDescription = GoogleFonts.josefinSans(
+    fontWeight: FontWeight.w400,
+    fontSize: 10,
+    color: const Color(0xff0B0B0B),
+  );
+  static TextStyle headingBookNow = GoogleFonts.josefinSans(
+    fontWeight: FontWeight.w600,
+    fontSize: 14,
+    color: const Color(0xffB5B5B5),
+  );
+  static TextStyle headingPaymentMethod = GoogleFonts.josefinSans(
+    fontWeight: FontWeight.w700,
+    fontSize: 12,
+    color: const Color(0xff0B0B0B),
+  );
+  static TextStyle headingPaymentDescription = GoogleFonts.josefinSans(
+    fontWeight: FontWeight.w500,
+    fontSize: 10,
+    color: const Color(0xff919191),
+  );
+  static TextStyle headingPaymentTime = GoogleFonts.josefinSans(
+    fontWeight: FontWeight.w600,
+    fontSize: 12,
+    color: const Color(0xff0B0B0B),
+  );
+  static TextStyle headingpaymentSucces = GoogleFonts.josefinSans(
+    fontWeight: FontWeight.w600,
+    fontSize: 28,
+    color: const Color(0xff0B0B0B),
+  );
+  static TextStyle headingAmountPaid = GoogleFonts.josefinSans(
+    fontWeight: FontWeight.w400,
+    fontSize: 14,
+    color: const Color(0xff030303),
+  );
+  static TextStyle headingRupiah = GoogleFonts.josefinSans(
+    fontWeight: FontWeight.w500,
+    fontSize: 24,
+    color: const Color(0xff030303),
+  );
+  static TextStyle headingChangePayment = GoogleFonts.josefinSans(
+    fontWeight: FontWeight.w500,
+    fontSize: 10,
+    color: const Color(0xff004040),
+  );
+}
