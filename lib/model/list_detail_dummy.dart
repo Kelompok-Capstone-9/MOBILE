ca95f589c286055f6badbf6929b76e7726723537
rouf_majidin <roufmajidin777@gmail.com>
2023-05-18 23:51:43 +0700

    add list dummy & color icon

---
+++ lib/model/list_detail_dummy.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+List<Icon> icon = const [
+  Icon(
+    Icons.star,
+    size: 14,
+    color: Color(0xffFFE600),
+  ),
+  Icon(
+    Icons.star,
+    size: 14,
+    color: Color(0xffFFE600),
+  ),
+  Icon(
+    Icons.star,
+    size: 14,
+    color: Color(0xffFFE600),
+  ),
+  Icon(
+    Icons.star,
+    size: 14,
+    color: Color(0xffFFE600),
+  ),
+  Icon(
+    Icons.star,
+    size: 14,
+    color: Color(0xff7D7D7D),
+  )
+];
+var package = [
+  {'harga': 65000, 'type': '/ Daily', 'status': false},
+  {'harga': 150000, 'type': '/ Weekly', 'status': false},
+  {'harga': 1800000, 'type': '/ Monthly', 'status': false}
+];
--- lib/themes/color_style.dart
+++ lib/themes/color_style.dart
@@ -3,6 +3,7 @@ import 'package:google_fonts/google_fonts.dart';
 
 class ColorsTheme {
   static final Color bgScreen = Color(0xffF6F6F6);
+  static final Color iconColor = Color(0xff00007A);
 }
 
 class ThemeText {
