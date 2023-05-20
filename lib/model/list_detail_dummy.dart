f1905525230036767e8f17c181704d6eaf0791be
rouf_majidin <roufmajidin777@gmail.com>
2023-05-21 02:47:14 +0700

    update list dummy & color style

--- lib/model/list_detail_dummy.dart
+++ lib/model/list_detail_dummy.dart
@@ -32,3 +32,27 @@ var package = [
   {'harga': 150000, 'type': '/ Weekly', 'status': false},
   {'harga': 1800000, 'type': '/ Monthly', 'status': false}
 ];
+
+var paymentMethode = [
+  {
+    'type': 'GO-PAY',
+    'value': 1,
+    'desc': 'Scan QR code with GO-JEK app',
+    'image': 'assets/images/gopay.png',
+    'onTap': 'false',
+  },
+  {
+    'type': 'Credit Card',
+    'value': 2,
+    'desc': 'Payment will be redirect to Midtrans',
+    'image': 'assets/images/credit-card.png',
+    'onTap': 'false',
+  },
+  {
+    'type': 'ShopeePay',
+    'value': 3,
+    'desc': 'Payment will be redirect to Shopee App',
+    'image': 'assets/images/ShopeePay.png',
+    'onTap': 'false',
+  },
+];
--- lib/themes/color_style.dart
+++ lib/themes/color_style.dart
@@ -4,6 +4,8 @@ import 'package:google_fonts/google_fonts.dart';
 class ColorsTheme {
   static final Color bgScreen = Color(0xffF6F6F6);
   static final Color iconColor = Color(0xff00007A);
+  static final Color primary600 = Color(0xffFF7F00);
+  static final Color colorLight = Color(0xffF6F6F6);
 }
 
 class ThemeText {
