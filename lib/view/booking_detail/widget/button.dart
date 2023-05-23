d358319ea1ff9a1a8267656f3cb406e482e9177a
rouf_majidin <roufmajidin777@gmail.com>
2023-05-23 14:04:48 +0700

    update button :)

--- lib/view/booking_detail/widget/button.dart
+++ lib/view/booking_detail/widget/button.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: must_be_immutable
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -28,6 +30,102 @@ class ButtonPay extends StatelessWidget {
                 color: ColorsTheme.colorLight),
           )),
     );
+  }
+}
+
+class ButtonFilledSmall extends StatelessWidget {
+  String textButton;
+  ButtonFilledSmall({super.key, required this.textButton});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        alignment: Alignment.center,
+        height: 38,
+        width: 176,
+        decoration: BoxDecoration(
+            color: const Color(0xffFF7F00),
+            borderRadius: BorderRadius.circular(10)),
+        child: Text(
+          textButton,
+          style: GoogleFonts.josefinSans(
+            fontWeight: FontWeight.w600,
+            fontSize: 14,
+            color: Colors.white,
+          ),
+        ));
+  }
+}
+
+class ButtonFilledSmallBackPayment extends StatelessWidget {
+  String textButton;
+  ButtonFilledSmallBackPayment({super.key, required this.textButton});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        alignment: Alignment.center,
+        height: 38,
+        width: 150,
+        decoration: BoxDecoration(
+            color: const Color(0xffFF7F00),
+            borderRadius: BorderRadius.circular(10)),
+        child: Text(
+          textButton,
+          style: GoogleFonts.josefinSans(
+            fontWeight: FontWeight.w600,
+            fontSize: 14,
+            color: Colors.white,
+          ),
+        ));
+  }
+}
+
+class ButtonOutlineSmallCancelPayment extends StatelessWidget {
+  String textButton;
+  ButtonOutlineSmallCancelPayment({super.key, required this.textButton});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        alignment: Alignment.center,
+        height: 38,
+        width: 150,
+        decoration: BoxDecoration(
+            color: const Color.fromARGB(0, 255, 255, 255),
+            borderRadius: BorderRadius.circular(10),
+            border: Border.all(color: const Color(0xffFF7F00))),
+        child: Text(
+          textButton,
+          style: GoogleFonts.josefinSans(
+              fontWeight: FontWeight.w600,
+              fontSize: 14,
+              color: const Color(0xffFF7F00)),
+        ));
+  }
+}
+
+class ButtonOutlineSmall extends StatelessWidget {
+  String textButton;
+  ButtonOutlineSmall({super.key, required this.textButton});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        alignment: Alignment.center,
+        height: 38,
+        width: 176,
+        decoration: BoxDecoration(
+            color: const Color.fromARGB(0, 255, 255, 255),
+            borderRadius: BorderRadius.circular(10),
+            border: Border.all(color: const Color(0xffFF7F00))),
+        child: Text(
+          textButton,
+          style: GoogleFonts.josefinSans(
+              fontWeight: FontWeight.w600,
+              fontSize: 14,
+              color: const Color(0xffFF7F00)),
+        ));
   }
 }
 
