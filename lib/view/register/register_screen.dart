e2dd26bc1b146bf9c9d9dce5de0fe5c44b2d2ab2
Risdiani <dinirisdi4@gmail.com>
2023-05-24 07:19:25 +0700

    update Register Screen

--- lib/view/register/register_screen.dart
+++ lib/view/register/register_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:gofit_apps/themes/color_style.dart';
+import 'package:gofit_apps/view/register/widget/decoration_form.dart';
 import 'package:gofit_apps/view/register/widget/validator_register.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -25,27 +26,13 @@ class _RegisterScreenState extends State<RegisterScreen> {
           children: [
             Padding(
               padding: const EdgeInsets.only(top: 66, bottom: 66),
-              child: Text(
-                'GoFit',
-                style: GoogleFonts.josefinSans(
-                    color: const Color(0xFFFF7F00),
-                    fontSize: 48,
-                    fontWeight: FontWeight.w700),
-                textAlign: TextAlign.justify,
-              ),
+              child: Text('GoFit', style: ThemeText.headingLabelGofit),
             ),
             Align(
               alignment: Alignment.bottomLeft,
               child: Padding(
                 padding: const EdgeInsets.only(left: 16, bottom: 16),
-                child: Text(
-                  'Sign Up',
-                  style: GoogleFonts.josefinSans(
-                      color: Colors.black,
-                      fontSize: 24,
-                      fontWeight: FontWeight.w700),
-                  textAlign: TextAlign.justify,
-                ),
+                child: Text('Sign Up', style: ThemeText.headingLogin),
               ),
             ),
             buildRegisterForm()
@@ -63,24 +50,9 @@ class _RegisterScreenState extends State<RegisterScreen> {
             children: [
               TextFormField(
                 controller: _nameController,
-                decoration: InputDecoration(
-                  fillColor: const Color(0xFFFFFFFF),
-                  prefixIcon: const Icon(
-                    Icons.person_2_outlined,
-                    color: Color(0xFFB5B5B5),
-                  ),
-                  labelText: 'Username',
-                  labelStyle: GoogleFonts.josefinSans(
-                      fontSize: 14, color: const Color(0xFFB5B5B5)),
-                  border: const OutlineInputBorder(),
-                  focusedBorder: const OutlineInputBorder(
-                    borderSide: BorderSide(color: Color(0xffFF7F00)),
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(4),
-                    ),
-                  ),
-                  floatingLabelBehavior: FloatingLabelBehavior.never,
-                ),
+                decoration: DecorationFormStyle.decorationForm(
+                    labelText: 'Username',
+                    prefixIcon: const Icon(Icons.person_outlined)),
                 onChanged: (value) {
                   setState(
                     () {
@@ -97,24 +69,9 @@ class _RegisterScreenState extends State<RegisterScreen> {
               const SizedBox(height: 16),
               TextFormField(
                 controller: _emailController,
-                decoration: InputDecoration(
-                  fillColor: const Color(0xFFFFFFFF),
-                  prefixIcon: const Icon(
-                    Icons.email_outlined,
-                    color: Color(0xFFB5B5B5),
-                  ),
-                  labelText: 'Email Address',
-                  labelStyle: GoogleFonts.josefinSans(
-                      fontSize: 14, color: const Color(0xFFB5B5B5)),
-                  border: const OutlineInputBorder(),
-                  focusedBorder: const OutlineInputBorder(
-                    borderSide: BorderSide(color: Color(0xffFF7F00)),
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(4),
-                    ),
-                  ),
-                  floatingLabelBehavior: FloatingLabelBehavior.never,
-                ),
+                decoration: DecorationFormStyle.decorationForm(
+                    labelText: 'Email Address',
+                    prefixIcon: const Icon(Icons.email_outlined)),
                 onChanged: (value) {
                   setState(() {
                     isFormFilled = _nameController.text.isNotEmpty &&
@@ -129,24 +86,9 @@ class _RegisterScreenState extends State<RegisterScreen> {
               const SizedBox(height: 16),
               TextFormField(
                 controller: _passwordController,
-                decoration: InputDecoration(
-                  fillColor: const Color(0xFFFFFFFF),
-                  prefixIcon: const Icon(
-                    Icons.lock_outline,
-                    color: Color(0xFFB5B5B5),
-                  ),
-                  labelText: 'Password',
-                  labelStyle: GoogleFonts.josefinSans(
-                      fontSize: 14, color: const Color(0xFFB5B5B5)),
-                  border: const OutlineInputBorder(),
-                  focusedBorder: const OutlineInputBorder(
-                    borderSide: BorderSide(color: Color(0xffFF7F00)),
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(4),
-                    ),
-                  ),
-                  floatingLabelBehavior: FloatingLabelBehavior.never,
-                ),
+                decoration: DecorationFormStyle.decorationForm(
+                    labelText: 'Password',
+                    prefixIcon: const Icon(Icons.lock_outline)),
                 onChanged: (value) {
                   setState(() {
                     isFormFilled = _nameController.text.isNotEmpty &&
@@ -155,29 +97,16 @@ class _RegisterScreenState extends State<RegisterScreen> {
                         _confirmpasswordController.text.isNotEmpty;
                   });
                 },
+                validator: (confirmPw) =>
+                    ConfirmPasswordValidator.validateConfirmPw(
+                        confirmPw, _passwordController.text),
               ),
               const SizedBox(height: 16),
               TextFormField(
                 controller: _confirmpasswordController,
-                decoration: InputDecoration(
-                  fillColor: const Color(0xFFFFFFFF),
-                  prefixIcon: const Icon(
-                    Icons.lock_outlined,
-                    color: Color(0xFFB5B5B5),
-                  ),
-                  labelText: 'Confirm Password',
-                  labelStyle: GoogleFonts.josefinSans(
-                      fontSize: 14, color: const Color(0xFFB5B5B5)),
-                  border: const OutlineInputBorder(),
-                  focusedBorder: const OutlineInputBorder(
-                    borderSide: BorderSide(color: Color(0xffFF7F00)),
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(4),
-                    ),
-                  ),
-                  floatingLabelBehavior: FloatingLabelBehavior.never,
-                  errorMaxLines: 3,
-                ),
+                decoration: DecorationFormStyle.decorationForm(
+                    labelText: 'Confirm Password',
+                    prefixIcon: const Icon(Icons.lock_outline)),
                 onChanged: (value) {
                   setState(() {
                     isFormFilled = _nameController.text.isNotEmpty &&
