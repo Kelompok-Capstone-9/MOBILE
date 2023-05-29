1226a66337e80e8eef8c66dde86e42dcd6f156cc
Abet Kalingga Wijaya <75027682+abetkalinggaw@users.noreply.github.com>
2023-05-29 22:19:10 +0700

    add file activities_screen.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:gofit_apps/view/activities/activities_screen.dart';
 import 'view/splash_screen.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -11,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SplashScreen(),
+      home: ActivitiesScreen(),
     );
   }
 }
