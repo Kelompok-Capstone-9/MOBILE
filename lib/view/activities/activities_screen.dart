93e2861cbbb4cddfa9ff7eafaf770d6ba93056a5
Abet Kalingga Wijaya <75027682+abetkalinggaw@users.noreply.github.com>
2023-05-30 11:59:12 +0700

    update style tab bar

--- lib/view/activities/activities_screen.dart
+++ lib/view/activities/activities_screen.dart
@@ -1,44 +1,88 @@
+// Import the necessary packages and libraries
 import 'package:flutter/material.dart';
 import 'package:gofit_apps/themes/color_style.dart';
 
-class ActivitiesScreen extends StatelessWidget {
+// Create a stateful widget for the screen
+class ActivitiesScreen extends StatefulWidget {
   const ActivitiesScreen({super.key});
+
+  @override
+  _ActivitiesScreenState createState() => _ActivitiesScreenState();
+}
+
+// Define the state for the screen
+class _ActivitiesScreenState extends State<ActivitiesScreen> {
+  int _currentIndex = 0;
+
+  // Create a list of tab bars
+  final List<Widget> _tabBars = [
+    const Tab(
+      text: 'On Progress',
+    ),
+    const Tab(
+      text: 'Finished',
+    ),
+  ];
+
+  // Create a list of tab bar views
+  final List<Widget> _tabBarViews = [
+    const OnProgressTab(),
+    const FinishedTab(),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-      length: 2,
+      length: _tabBars.length,
       child: Scaffold(
-        backgroundColor: ColorsTheme.bgScreen,
         appBar: AppBar(
           title: Text(
-            'Activities',
+            'Tab Screen',
             style: ThemeText.heading1,
           ),
           backgroundColor: ColorsTheme.bgScreen,
           elevation: 0,
-        ),
-        body: Column(
-          children: [
-            TabBar(
-              labelColor: Colors.black,
-              labelStyle: ThemeText.heading1,
-              indicator: BoxDecoration(
-                color: Colors.orange,
-              ),
-              unselectedLabelColor: Colors.grey,
-              tabs: const [
-                Tab(
-                  text: 'On Progress',
-                ),
-                Tab(
-                  text: 'Finished',
-                ),
-              ],
+          bottom: TabBar(
+            labelStyle: ThemeText.heading1,
+            labelColor: Colors.black,
+            unselectedLabelColor: Colors.grey[600],
+            indicator: BoxDecoration(
+              color: Colors.orange,
             ),
-          ],
+            onTap: (index) {
+              setState(() {
+                _currentIndex = index;
+              });
+            },
+            tabs: _tabBars,
+          ),
         ),
+        body: _tabBarViews[_currentIndex],
       ),
+    );
+  }
+}
+
+// Define the home tab view
+class OnProgressTab extends StatelessWidget {
+  const OnProgressTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('This is the Home Tab'),
+    );
+  }
+}
+
+// Define the settings tab view
+class FinishedTab extends StatelessWidget {
+  const FinishedTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('This is the Settings Tab'),
     );
   }
 }
