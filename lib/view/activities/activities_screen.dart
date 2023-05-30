f03cd71ba8e680989cba34a6dc5010e18eac0a46
Abet Kalingga Wijaya <75027682+abetkalinggaw@users.noreply.github.com>
2023-05-30 11:41:36 +0700

    add style activities screen

--- lib/view/activities/activities_screen.dart
+++ lib/view/activities/activities_screen.dart
@@ -1,10 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:gofit_apps/themes/color_style.dart';
 
 class ActivitiesScreen extends StatelessWidget {
   const ActivitiesScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        backgroundColor: ColorsTheme.bgScreen,
+        appBar: AppBar(
+          title: Text(
+            'Activities',
+            style: ThemeText.heading1,
+          ),
+          backgroundColor: ColorsTheme.bgScreen,
+          elevation: 0,
+        ),
+        body: Column(
+          children: [
+            TabBar(
+              labelColor: Colors.black,
+              labelStyle: ThemeText.heading1,
+              indicator: BoxDecoration(
+                color: Colors.orange,
+              ),
+              unselectedLabelColor: Colors.grey,
+              tabs: const [
+                Tab(
+                  text: 'On Progress',
+                ),
+                Tab(
+                  text: 'Finished',
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
