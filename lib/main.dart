2f4580e5287d0f9748babf23004893a41adc6066
rouf_majidin <roufmajidin777@gmail.com>
2023-05-31 00:34:57 +0700

    fix --

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'package:gofit_apps/view/register/register_screen.dart';
+import 'package:gofit_apps/view/splash_screen/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,6 +12,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-        debugShowCheckedModeBanner: false, home: RegisterScreen());
+        debugShowCheckedModeBanner: false, home: SplashScreen());
   }
 }
