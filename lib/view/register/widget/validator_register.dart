8e8fd78f1a83cef71c11026af21b27b6fe470f84
rouf_majidin <roufmajidin777@gmail.com>
2023-06-05 15:29:55 +0700

    update : c3

--- lib/view/register/widget/validator_register.dart
+++ lib/view/register/widget/validator_register.dart
@@ -20,8 +20,6 @@ class EmailValidatorSignup {
     RegExp regex = RegExp(emailPattern);
     if (!regex.hasMatch(email)) {
       return 'Please enter a valid email address.';
-    } else if (email == email) {
-      return 'Email address is already registered.';
     }
     return null;
   }
@@ -29,15 +27,18 @@ class EmailValidatorSignup {
 
 class ConfirmPasswordValidator {
   static String? validateConfirmPw(String? confirmPw, String password) {
+    String pattern =
+        r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$';
+    RegExp regExp = new RegExp(pattern);
+    ;
     if (confirmPw == null || confirmPw.isEmpty) {
       return 'Password is required';
     } else if (password != confirmPw) {
       return 'The password confirmation does not match';
-    } else if (!RegExp(
-            r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!_@#\$%^&])[A-Za-z\d!_@#\$%^&]{8,}$')
-        .hasMatch(password)) {
-      return 'Password must have at least 8 characters that include at least 1 lowercase character, 1 uppercase character, 1 number, and 1 special character in (!_@#\$%^&)';
+    } else if (!regExp.hasMatch(password)) {
+      return 'Password should contain 8 character (?=./*, etc) ';
+    } else {
+      return null;
     }
-    return null;
   }
 }
