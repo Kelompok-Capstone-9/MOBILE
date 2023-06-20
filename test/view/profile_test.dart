d03197e2b769266e45cae7cccfa2f44aacfb348f
Rohimah Febrianti <imafbr21@gmail.com>
2023-06-21 00:49:45 +0700

    add: profile_test

---
+++ test/view/profile_test.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:gofit_apps/view/profile/profile_screen.dart';
+import 'package:gofit_apps/view/register/register_screen.dart';
+import 'package:gofit_apps/view_model/login_provider.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  group('profile', () {
+    testWidgets('Test text pada Profile', (WidgetTester tester) async {
+      await tester.pumpWidget(
+        ChangeNotifierProvider(
+          create: (_) => LoginProvider(),
+          child: const MaterialApp(
+            home: ProfileScreen(),
+          ),
+        ),
+      );
+      Finder nameField = find.byType(Text);
+      expect(nameField, findsNWidgets(12));
+    });
+
+    testWidgets('Test icon', (WidgetTester tester) async {
+      await tester.pumpWidget(
+        ChangeNotifierProvider(
+          create: (_) => LoginProvider(),
+          child: const MaterialApp(
+            home: ProfileScreen(),
+          ),
+        ),
+      );
+      Finder icon = find.byType(Icon);
+      expect(icon, findsNWidgets(3));
+    });
+
+    testWidgets('Test button logout', (WidgetTester tester) async {
+      await tester.pumpWidget(
+        ChangeNotifierProvider(
+          create: (_) => LoginProvider(),
+          child: const MaterialApp(
+            home: ProfileScreen(),
+          ),
+        ),
+      );
+
+      Finder logoutButton = find.byType(ElevatedButton);
+      expect(logoutButton, findsOneWidget);
+    });
+
+    testWidgets('Test circle avatar', (WidgetTester tester) async {
+      await tester.pumpWidget(
+        ChangeNotifierProvider(
+          create: (_) => LoginProvider(),
+          child: const MaterialApp(
+            home: ProfileScreen(),
+          ),
+        ),
+      );
+
+      Finder circle = find.byType(CircleAvatar);
+      expect(circle, findsOneWidget);
+    });
+
+    testWidgets('Test image', (WidgetTester tester) async {
+      await tester.pumpWidget(
+        ChangeNotifierProvider(
+          create: (_) => LoginProvider(),
+          child: const MaterialApp(
+            home: ProfileScreen(),
+          ),
+        ),
+      );
+
+      Finder image = find.byType(Image);
+      expect(image, findsOneWidget);
+    });
+
+    testWidgets('Test button news', (WidgetTester tester) async {
+      await tester.pumpWidget(
+        ChangeNotifierProvider(
+          create: (_) => LoginProvider(),
+          child: const MaterialApp(
+            home: ProfileScreen(),
+          ),
+        ),
+      );
+
+      Finder newsButton = find.byType(Switch);
+      expect(newsButton, findsOneWidget);
+    });
+  });
+}
