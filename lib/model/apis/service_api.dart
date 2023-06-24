a0f2a610126397f70056b7320b289de14e628b5d
rouf_majidin <roufmajidin777@gmail.com>
2023-06-25 02:21:32 +0700

    -

--- lib/model/apis/service_api.dart
+++ lib/model/apis/service_api.dart
@@ -11,7 +11,7 @@ import 'package:gofit_apps/model/register.dart';
 import 'package:gofit_apps/model/news_letter.dart';
 import 'dart:developer';
 import 'package:http/http.dart' as http;
-import 'package:mime/mime.dart';
+// import 'package:mime/mime.dart';
 import '../booking.dart';
 import 'package:http_parser/http_parser.dart';
 
