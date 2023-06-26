dfc805fbba59000b4a44a74167ec56808c25aa80
Rohimah Febrianti <imafbr21@gmail.com>
2023-06-26 13:04:36 +0700

    update: activities test

--- test/view/activities_test.dart
+++ test/view/activities_test.dart
@@ -1,23 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:gofit_apps/view/activities/activities_screen.dart';
+import 'package:gofit_apps/view_model/booking_provider.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   group('Activities', () {
     testWidgets('Test text Activities', (WidgetTester tester) async {
       await tester.pumpWidget(
-        const MaterialApp(
-          home: ActivitiesScreen(),
+        ChangeNotifierProvider(
+          create: (_) => BookingProvider(),
+          child: const MaterialApp(
+            home: ActivitiesScreen(),
+          ),
         ),
       );
       Finder textField = find.byType(Text);
-      expect(textField, findsNWidgets(11));
+      expect(textField, findsNWidgets(3));
     });
 
     testWidgets('Test Tab', (WidgetTester tester) async {
       await tester.pumpWidget(
-        const MaterialApp(
-          home: ActivitiesScreen(),
+        ChangeNotifierProvider(
+          create: (_) => BookingProvider(),
+          child: const MaterialApp(
+            home: ActivitiesScreen(),
+          ),
         ),
       );
       final tabBar = find.byType(TabBar);
